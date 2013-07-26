8b8498d9979cf9a38dac76e4128c919e6f9a3bde
Daniel Mack <zonque@gmail.com>
2013-07-26 18:16:06 +0200

    cleanup main.c

--- main.c
+++ main.c
@@ -8,22 +8,21 @@ extern __u32 _binary_input_zImage_start;
 
 void main(__u32 dummy, __u32 machid, const struct tag *tags)
 {
-	__u32 system_rev = 0;
 	const struct tag *t;
-	void (*start_kernel)(__u32 dummy, __u32 machid, void *dtb);
-	void *zimage = &_binary_input_zImage_start;
-	struct board *board = NULL;
+	struct board *board;
+	__u32 system_rev = 0;
+	void (*start_kernel)(__u32 dummy, __u32 machid, void *dtb) =
+		(void *) &_binary_input_zImage_start;
 
 	putstr("++ Impedance Matcher (3rd stage loader) ++\n");
 
 	/* walk the atags to determine the system revision */
-	for_each_tag(t, tags) {
+	for_each_tag(t, tags)
 		switch (t->hdr.tag) {
 			case ATAG_REVISION:
 				system_rev = t->u.rev.rev;
 				break;
 		}
-	}
 
 	board = match_board(machid, system_rev & 0xff);
 	if (!board) {
@@ -42,8 +41,5 @@ void main(__u32 dummy, __u32 machid, const struct tag *tags)
 	putstr("\n");
 
 	putstr("Booting into Linux kernel ...\n");
-
-	start_kernel = zimage;
 	start_kernel(0, 0xffffffff, board->dtb);
 }
-
