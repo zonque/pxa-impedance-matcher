1e29caa6729b3fc253caff224634d7416651c82c
Piotr Wilczek <p.wilczek@samsung.com>
2013-12-18 13:09:04 +0100

    registers: add write u8 type
    Signed-off-by: Piotr Wilczek <p.wilczek@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- register.h
+++ register.h
@@ -3,6 +3,9 @@
 
 #include "types.h"
 
+void writeb(u8, u32);
+u8 readb(u32);
+
 void writel(u32, u32);
 u32 readl(u32);
 
--- types.h
+++ types.h
@@ -1,6 +1,7 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
+typedef unsigned char u8;
 typedef unsigned int u32;
 typedef unsigned long long u64;
 #define NULL	((void *) 0)
