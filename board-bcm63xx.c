48dcf9424be7b4982d96375f7884db924b074c2d
Florian Fainelli <f.fainelli@gmail.com>
2015-07-29 12:35:44 -0700

    boards: add Broadcom BCM63xx board code support
    Read the CHIP_ID value from the INT_PERIPH_ID register and match a
    generic "brcm,bcm63138" entry with DTBs that have been built-into the
    impedance matcher.
    
    In the future, we will probably need to read the boot strap to figure
    out the boot device and read the board id from the NVRAM and match it
    exactly with a collection of DTBs.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

---
+++ board-bcm63xx.c
@@ -0,0 +1,74 @@
+#include "atags.h"
+#include "board.h"
+#include "dtbs.h"
+#include "print.h"
+#include "register.h"
+#include "types.h"
+
+extern u32 _binary_input_zImage_start;
+extern u32 _binary_dtbs_bin_start;
+
+struct board board;
+
+struct bcm63xx_board {
+	u32		chip_id;
+	const char	*compatible;
+};
+
+static struct bcm63xx_board boards[] = {
+	{
+		.chip_id	= 0x63138,
+		.compatible	= "brcm,bcm63138",
+	},
+	{ 0, NULL }	/* sentinel */
+};
+
+static void panic(void)
+{
+	while (1)
+		;
+}
+
+/* Physical address of the INT_REVID register */
+#define INT_REVID_REG	0xfffe8000
+#define  CHIP_ID_SHIFT	12
+
+static u32 get_chip_id()
+{
+	return readl(INT_REVID_REG) >> CHIP_ID_SHIFT;
+}
+
+struct board *match_board(u32 machid, const struct tag *tags)
+{
+	struct bcm63xx_board *bcm_board;
+	u32 chip_id;
+
+	chip_id = get_chip_id();
+
+	putstr("Found a Broadcom BCM");
+	printhex(chip_id);
+	putstr(" chip\n");
+
+	for (bcm_board = boards; bcm_board->chip_id; bcm_board++) {
+		if (bcm_board->chip_id == chip_id)
+			break;
+	}
+
+	if (bcm_board->compatible == NULL) {
+		putstr("ERROR MATCHING BOARD!\n");
+		panic(); /* doesn't return */
+	}
+
+	board.kernel = &_binary_input_zImage_start;
+	board.compatible = bcm_board->compatible;
+	board.dtb = find_dtb(&_binary_dtbs_bin_start, bcm_board->compatible);
+
+	if (board.dtb == NULL) {
+		putstr("NO DTB BLOB FOUND FOR ");
+		putstr(bcm_board->compatible);
+		putstr("\n");
+		panic(); /* doesn't return */
+	}
+
+	return &board;
+}
