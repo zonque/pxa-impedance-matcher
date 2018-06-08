3ed6a4699c03d4e5966f813d9b7b306f78433a98
Daniel Mack <daniel@zonque.org>
2018-06-08 17:28:21 +0200

    register: remove 'inline' keyword from functions
    No idea how this has ever worked for anyone?

--- register.c
+++ register.c
@@ -1,21 +1,21 @@
 #include "types.h"
 
-inline void writeb(u8 val, u32 addr)
+void writeb(u8 val, u32 addr)
 {
 	*(volatile u32 *)addr = val;
 }
 
-inline u8 readb(u32 addr)
+u8 readb(u32 addr)
 {
 	return *(volatile u32 *)addr;
 }
 
-inline void writel(u32 val, u32 addr)
+void writel(u32 val, u32 addr)
 {
 	*(volatile u32 *) addr = val;
 }
 
-inline u32 readl(u32 addr)
+u32 readl(u32 addr)
 {
 	return *(volatile u32 *) addr;
 }
