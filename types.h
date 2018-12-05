f0768dbec78281734a4fca6dca43e69e76a69182
Daniel Mack <daniel@zonque.org>
2018-12-05 22:14:23 +0100

    types.h: drop definitions that can be used from stddef.h

--- types.h
+++ types.h
@@ -1,6 +1,8 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
+#include <stddef.h>
+
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
@@ -8,10 +10,6 @@ typedef unsigned long long u64;
 
 typedef unsigned int	uint32_t;
 typedef unsigned long	long uint64_t;
-
-typedef unsigned long	uintptr_t;
-
-typedef unsigned long size_t;
 
 #define cpu_to_be32(x) ((u32)(x))
 #define cpu_to_be64(x) ((u64)(x))
@@ -34,7 +32,5 @@ typedef unsigned long size_t;
 	(((u64)(x) & (u64)0x0000ff0000000000ULL) >> 24) |   \
 	(((u64)(x) & (u64)0x00ff000000000000ULL) >> 40) |   \
 	(((u64)(x) & (u64)0xff00000000000000ULL) >> 56)))
-
-#define NULL	((void *) 0)
 
 #endif /* _TYPES_H */
